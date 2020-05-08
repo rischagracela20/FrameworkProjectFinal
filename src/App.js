af3b3a42b1f10d38f57903e4a0065dd24d1e5985
rischagracela20 <gracelarischa20@gmail.com>
2020-05-09 00:11:46 +0800

    ProjectFinalCovidTracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, PilihNegara } from './components'
+import styles from './App.module.css'
+import { fetchData, fetchFirstCountry } from './api'
+
+
+class App extends React.Component {
+    state = {
+        data: {
+
+        },
+        dataCountry: {
+
+        },
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData()
+        this.setState({ data: fetchedData })
+        const firstCountry = await fetchFirstCountry()
+        const firstData = await fetchData(firstCountry)
+        this.setState({ dataCountry: firstData, country: firstCountry })
+
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country)
+        this.setState({ dataCountry: fetchedData, country: country })
+    }
+
+      
+    render() {
+        return (
+            <div className={styles.container}>
+                <center><h1>COVID-19 TRACKER</h1>
+                <h4 style={{color: 'blue'}}><u><i>#StayHome</i></u> & <u><i>#StopTheSpread</i></u></h4></center>
+                <Cards data={this.state.data} />
+                <center><PilihNegara handleCountryChange={this.handleCountryChange} /></center>
+                <Cards data={this.state.dataCountry} country={this.state.country} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+import styles from './Cards.module.css';
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate }, country }) => {
+  if (!confirmed) {
+    return 'Loading. Please wait...';
+  }
+
+  return (
+    <div className={styles.container}>
+      <br />
+      <h2><font size = '5'>{country ? `COUNTRIES: ${country}` : `GLOBAL`}</font></h2>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Positive.
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Number of active cases of COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered.
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={recovered.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Number of recoveries from COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths.
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={deaths.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Number of deaths caused by COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default Info;
